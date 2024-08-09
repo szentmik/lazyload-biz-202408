60dcce9395761ee6b52d344a7d2a7e0f89eae6e5
Adam Szentmiklossy <szentmik@gmail.com>
2024-08-09 15:17:42 +0200

    final

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// toggle switch 
+const mMenuBtn = document.querySelector(".mobile-menu-btn");
+const navClose = document.querySelector(".nav-close");
+const toggleSwitch = document.querySelectorAll(".slide-btns");
+const mMenuLinks = document.querySelectorAll(".nav-link");
+const langChange = document.querySelectorAll(".lang-dropdown-content a");
+
+toggleSwitch.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        toggleSwitch[i].children[0].classList.toggle("slide-btn-active");
+        toggleSwitch[i].children[1].classList.toggle("slide-btn-active");
+
+    });
+});
+
+// mobile menu 
+
+mMenuBtn.addEventListener("click", () => {
+    document.querySelector(".navbar-links-bg").classList.add("show-nav-links");
+    mMenuBtn.classList.add("desktop-hide");
+    console.log("click");
+
+});
+
+navClose.addEventListener("click", () => {
+    mMenuBtn.classList.remove("desktop-hide");
+    document.querySelector(".navbar-links-bg").classList.remove("show-nav-links");
+});
+
+mMenuLinks.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        mMenuBtn.classList.remove("desktop-hide");
+        document.querySelector(".navbar-links-bg").classList.remove("show-nav-links");
+    });
+});
+
+const langDropdown = () => {
+    document.querySelector(".lang-dropdown-content").classList.toggle("lang-show");
+};
